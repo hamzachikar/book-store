d113aab45e8ee159cc8086eb370f99f5bfaa7ebb
hamzachikar <chikar.h@outlook.fr>
2021-02-16 08:58:56 +0100

    adding swagger and some updates

--- src/main/java/com/app/store/entity/Book.java
+++ src/main/java/com/app/store/entity/Book.java
@@ -1,5 +1,8 @@
 package com.app.store.entity;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 
 import javax.persistence.*;
@@ -8,14 +11,17 @@ import java.util.UUID;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
     @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid",strategy = "uuid")
     private UUID Id;
     @Column
     private String name;
     @OneToMany(cascade = CascadeType.REMOVE,fetch = FetchType.LAZY)
     private List<Comment> comments;
-
     public void addComment(Comment comment) {
         this.comments.add(comment);
     }
---
+++ src/test/java/com/app/store/BookServiceTest.java
@@ -0,0 +1,7 @@
+package com.app.store;
+
+import org.junit.jupiter.api.Test;
+
+public class BookServiceTest {
+
+}
