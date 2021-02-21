ff96fb30a270de06ec67b1d088bb0bed45e06fa4
ichrak ben abdllah <ichrak.benabdallah@polytechnicien.tn>
2021-02-21 12:33:45 +0100

    adding maven_checkstyle in pom.xml // updating swaggerConfig

--- src/main/java/com/app/store/config/SwaggerConfig.java
+++ src/main/java/com/app/store/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.app.store.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -10,7 +12,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2);
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 }
